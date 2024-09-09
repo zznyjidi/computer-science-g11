808a44195a772e3f1d76f5679ef95419b4303262
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-09-10 01:36:18 +0800

    Add Practice

---
+++ CS-G11-Unit1/src/EscapeChars.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+public class EscapeChars {
+
+	public static void main(String[] args) {
+		System.out.println("I’m having trouble studying. ");
+		System.out.println("I think that \"ICS3U1\" is the best. ");
+		System.out.println("The path is C:\\\\temp\\\\data");
+		System.out.println("1\n2\n3\n4");
+		System.out.println("Friend1\tFriend2\tFriend3");
+	}
+
+}
---
+++ CS-G11-Unit1/src/TicTacToeBoard.java
@@ -0,0 +1,18 @@
+
+public class TicTacToeBoard {
+
+	public static void main(String[] args) {
+		System.out.println(
+				"=== Tic - Tac - Toe ===\n" + 
+				"\n" +
+				"\tX | O | O\n" +
+				"\t---------\n" +
+				"\tX | X | O\n" +
+				"\t---------\n" +
+				"\tO | O | X\n" +
+				"\n" +
+				"X wins ! Too bad O. " 
+				);
+	}
+
+}
