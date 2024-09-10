d808a1a8a134ece3877be190559d0652082b0c1d
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-09-11 01:23:39 +0800

    Add Variable Practice

---
+++ CS-G11-Unit1/src/Variables.java
@@ -0,0 +1,25 @@
+/**
+ * Declare & Init & Use Variables
+ */
+public class Variables {
+
+	public static void main(String[] args) {
+		String name = "Zixuan";
+		int age = 15;
+		double height = 170;
+		String dateOfBirth = "2008-09-23";
+		String favoriteColor = "#7D87D8";
+		String dreamCar = "unknown";
+		String favoriteVideoGame = "osu!";
+
+		System.out.println(
+				"I'm " + name + ". \n" +
+				"I'm " + age + " years old. \n" +
+				"I'm " + height + " cm tall. \n" +
+				"My birthday is " + dateOfBirth + ". \n" +
+				"My favorite color is " + favoriteColor + ". \n" +
+				"My dream car is " + dreamCar + ". \n" +
+				"My favorite video game is " + favoriteVideoGame + ". ");
+	}
+
+}
