353e948489d2e962a0558713d37fe94ddf706d1f
Zixuan Zhao <zixuan.zhao@zzny.tech>
2024-09-12 00:41:32 -0400

    Add Comments

--- CS-G11-Unit1/src/NumberTable.java
+++ CS-G11-Unit1/src/NumberTable.java
@@ -1,13 +1,19 @@
-
+/**
+ * Print Number Power Table
+ */
 public class NumberTable {
 
 	public static void main(String[] args) {
+		// Print Header
 		System.out.println("number\tsquare\tcube");
+		// Print Values
 		printTableLine(0, 10);
 	}
 
 	private static void printTableLine(int currentNumber, int endNumber) {
+		// Print Current Line
 		System.out.println(currentNumber + "\t" + (int)Math.pow(currentNumber, 2) + "\t" + (int)Math.pow(currentNumber, 3));
+		// Call printTableLine for next line if endNumber is not reached
 		if (currentNumber < endNumber) {
 			printTableLine(currentNumber+1, endNumber);
 		}
