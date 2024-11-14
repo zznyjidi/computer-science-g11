d7217d3314957acf1491b4c8fdd050df451501e6
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-11-14 13:12:09 -0500

    2024-11-14 Unit3 Performance Task

---
+++ Unit3Performance/src/ATLA.java
@@ -0,0 +1,55 @@
+/**
+ * Avatar the Last Airbender Character
+ * By Zixuan Zhao
+ */
+public class ATLA {
+
+    // Fields
+    String characterName;
+    int age;
+    String nation;
+
+    // Constructor
+    public ATLA(String characterName, int age, String nation) {
+        this.characterName = characterName;
+        setAge(age);
+        setNation(nation);
+    }
+
+    // Getters and Setters
+    public String getCharacterName() {
+        return characterName;
+    }
+
+    public void setCharacterName(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0)
+            this.age = age;
+        else
+            System.err.println("Age can't be negative! ");
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public void setNation(String nation) {
+        if (nation == "Earth" || nation == "Fire" || nation == "Water" || nation == "Air")
+            this.nation = nation;
+        else
+            System.err.println("Nation must be Earth, Fire, Water or Air! ");
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "ATLA [First Name=" + characterName + ", Age=" + age + ", Nation=" + nation + "]";
+    }
+}
---
+++ Unit3Performance/src/Application.java
@@ -0,0 +1,89 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * Create and Display a Array of Avatar the Last Airbender Characters
+ * and search the array by Nation and Age.
+ * By Zixuan Zhao
+ */
+public class Application {
+    public static void main(String[] args) {
+        // Create Array
+        ATLA[] characterArray = new ATLA[8];
+
+        // Fill, sort and display the Array
+        fillArray(characterArray);
+        sortArrayByAge(characterArray);
+        displayTitle();
+        displayArray(characterArray);
+
+        // Create Scanner
+        Scanner scan = new Scanner(System.in);
+
+        // Find characters with the same nation
+        System.out.print("Enter nation to see how many characters share the same nation: ");
+        String targetNation = scan.nextLine();
+        int characterCountByNation = countByNation(characterArray, targetNation);
+        System.out.printf("%d characters are form the %s nation. %n", characterCountByNation, targetNation);
+
+        // Find characters with the same age
+        System.out.print("Enter age to see how many characters are the same age: ");
+        int targetAge = scan.nextInt();
+        int characterCountByAge = countByAge(characterArray, targetAge);
+        System.out.printf("%d characters are %d years old. %n", characterCountByAge, targetAge);
+
+        // Close the Scanner
+        scan.close();
+    }
+
+    // Fill Array
+    private static void fillArray(ATLA[] characterArray) {
+        characterArray[0] = new ATLA("Zuko", 16, "Fire");
+        characterArray[1] = new ATLA("Suki", 15, "Earth");
+        characterArray[2] = new ATLA("Toph", 12, "Earth");
+        characterArray[3] = new ATLA("Azula", 14, "Fire");
+        characterArray[4] = new ATLA("Sokka", 15, "Water");
+        characterArray[5] = new ATLA("Katara", 14, "Water");
+        characterArray[6] = new ATLA("Aang", 12, "Air");
+        characterArray[7] = new ATLA("Iroh", 55, "Fire");
+    }
+
+    // Show Title
+    private static void displayTitle() {
+        System.out.println("ATLA CHARACTERS");
+        System.out.println("================");
+    }
+
+    // Sort ATLA Array by Age
+    private static void sortArrayByAge(ATLA[] characterArray) {
+        Arrays.sort(characterArray, Comparator.comparing(ATLA::getAge));
+    }
+
+    // Print Array
+    private static void displayArray(ATLA[] characterArray) {
+        for (ATLA character : characterArray) {
+            System.out.println(character);
+        }
+    }
+
+    // Find the number of Character with the same Nation
+    private static int countByNation(ATLA[] characterArray, String targetNation) {
+        int count = 0;
+        for (ATLA character : characterArray) {
+            if (character.getNation().equalsIgnoreCase(targetNation))
+                count++;
+        }
+        return count;
+    }
+
+    // Find the number of Character with the same Age
+    private static int countByAge(ATLA[] characterArray, int targetAge) {
+        int count = 0;
+        for (ATLA character : characterArray) {
+            if (character.getAge() == targetAge)
+                count++;
+        }
+        return count;
+    }
+}
