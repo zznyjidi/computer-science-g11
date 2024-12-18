55c4a02e01211618b18f3ad7fe0a5780c08c89d2
shift.0x7d8 <zixuan.zhao@ms.zzny.fun>
2024-12-17 20:11:02 -0500

    temp: add account login

---
+++ SuperMario/src/network/HttpRequest.java
@@ -0,0 +1,84 @@
+package network;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.Map;
+
+public class HttpRequest {
+    static final String BOUNDARY = "*****";
+    static final String NEWLINE = "\r\n";
+    static final String TWO_HYPHENS = "--";
+
+    public static HttpURLConnection post(URL requestUrl, Map<String, Object> bodyValues) throws IOException {
+        HttpURLConnection connection = (HttpURLConnection) requestUrl.openConnection();
+        // Set Request Info
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty(
+            "Content-Type", "multipart/form-data;boundary=" + HttpRequest.BOUNDARY);
+        // Send Request
+        // https://blog.morizyun.com/blog/android-httpurlconnection-post-multipart/index.html
+        DataOutputStream request = new DataOutputStream(connection.getOutputStream());
+        // Map to Request Body
+        // https://www.baeldung.com/java-hashmap-different-value-types
+        bodyValues.forEach((key, value) -> {
+            try {
+                request.writeBytes(HttpRequest.TWO_HYPHENS + HttpRequest.BOUNDARY + HttpRequest.NEWLINE);
+                request.writeBytes(String.format(
+                    "Content-Disposition: form-data; name=\"%s\"%s", 
+                    key, HttpRequest.NEWLINE
+                ));
+                request.writeBytes("Content-Type: text/plain; charset=UTF-8" + HttpRequest.NEWLINE);
+                request.writeBytes(HttpRequest.NEWLINE);
+                request.writeBytes(value + HttpRequest.NEWLINE);
+                request.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        // End Request Body
+        request.writeBytes(HttpRequest.NEWLINE);
+        request.writeBytes(HttpRequest.TWO_HYPHENS + HttpRequest.BOUNDARY + HttpRequest.TWO_HYPHENS + HttpRequest.NEWLINE);
+        request.flush();
+        request.close();
+
+        // Return Connection
+        return connection;
+    }
+
+    public static String getRespond(HttpURLConnection connection) throws IOException {
+        // Read Respond from connection
+        InputStream responseStream = new BufferedInputStream(connection.getInputStream());
+        BufferedReader responseStreamReader = new BufferedReader(new InputStreamReader(responseStream));
+
+        String line = "";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        while ((line = responseStreamReader.readLine()) != null) {
+            stringBuilder.append(line).append("\n");
+        }
+        responseStreamReader.close();
+        connection.disconnect();
+
+        return stringBuilder.toString();
+    }
+
+    public static void main(String[] args) {
+        try {
+            String respond = getRespond(HttpRequest.post(new URI("http://127.0.0.1:8080/return/403").toURL(), Map.ofEntries(
+                Map.entry("key1", "value1"),
+                Map.entry("key2", "value2")
+            )));
+            System.out.println(respond);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- SuperMario/src/online/Account.java
+++ SuperMario/src/online/Account.java
@@ -1,11 +1,16 @@
 package online;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Map;
 
 import global.Settings;
+import network.HttpRequest;
 
 public class Account {
     private int uid;
@@ -33,18 +38,26 @@ public class Account {
 
     public int login(String password) {
         try {
+            // Connect to Server
             URL url = new URI(
                 Settings.scoreServerAddr[0], 
                 Settings.scoreServerAddr[1],
-                Settings.scoreServerFeature.get("login"),
+                Settings.scoreServerFeature.get("user-login"),
                 null, null
             ).toURL();
+            HttpURLConnection connection = HttpRequest.post(url, Map.ofEntries(
+                Map.entry("username", username),
+                Map.entry("password", password)
+            ));
         } catch (MalformedURLException e) {
             // Protocol Not Supported
             return -2;
         } catch (URISyntaxException e) {
             // Invalid URI
             return -2;
+        } catch (IOException e) {
+            // Wrong Info
+            return -1;
         }
         return uid;
     }
